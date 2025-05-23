2957374a974faac014e331d49a94ac827d038066
Austin Varela <austinv11@gmail.com>
2025-05-23 15:34:00 -0400

    More explicit docker paths

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,6 +9,6 @@ else # biohpc
     docker_exec="docker1"
 fi
 
-$docker_exec build -t 10xrangers 10xrangers/
-$docker_exec build -t multiqc multiqc/
-$docker_exec build -t samtools samtools/
+$docker_exec build -t 10xrangers f"$(pwd)/10xrangers/"
+$docker_exec build -t multiqc f"$(pwd)multiqc/"
+$docker_exec build -t samtools f"$(pwd)samtools/"
