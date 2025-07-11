6956c6ec9d8df643eb2c35964bd847589458b7b4
Austin Varela <austinv11@gmail.com>
2025-07-11 09:48:36 -0400

    Fix syntax error

--- workflows/cnv_calling/split_by_sample.py
+++ workflows/cnv_calling/split_by_sample.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import scanpy as sc
 
 adata = sc.read_h5ad(snakemake.input.input_file)
@@ -5,5 +7,5 @@ adata = sc.read_h5ad(snakemake.input.input_file)
 # Split the AnnData object by sample
 for sample_name in adata.obs[snakemake.params.sample_column].unique():
     sample_adata = adata[adata.obs[snakemake.params.sample_column] == sample_name].copy()
-    output_file = snakemake.output.h5ad_files / f"{sample_name}.h5ad"
+    output_file = Path(snakemake.output.h5ad_files) / f"{sample_name}.h5ad"
     sample_adata.write_h5ad(output_file)
