9dbdb8190ab830e74d5f1e857940c7cb4a26831e
Austin Varela <austinv11@gmail.com>
2025-07-11 11:01:19 -0400

    Create parent dirs

--- workflows/cnv_calling/split_by_sample.py
+++ workflows/cnv_calling/split_by_sample.py
@@ -4,8 +4,10 @@ import scanpy as sc
 
 adata = sc.read_h5ad(snakemake.input.input_file)
 
+output_dir = Path(snakemake.output.h5ad_files)
+output_dir.mkdir(parents=True, exist_ok=True)
 # Split the AnnData object by sample
 for sample_name in adata.obs[snakemake.params.sample_column].unique():
     sample_adata = adata[adata.obs[snakemake.params.sample_column] == sample_name].copy()
-    output_file = Path(snakemake.output.h5ad_files) / f"{sample_name}.h5ad"
+    output_file = output_dir / f"{sample_name}.h5ad"
     sample_adata.write_h5ad(output_file)
