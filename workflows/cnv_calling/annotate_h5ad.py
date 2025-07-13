805a536951b0221b9c0055bb957b1fe318e3b629
Austin Varela <austinv11@gmail.com>
2025-07-13 10:11:49 -0400

    Fix the annotation of anndatas

--- workflows/cnv_calling/annotate_h5ad.py
+++ workflows/cnv_calling/annotate_h5ad.py
@@ -12,15 +12,17 @@ state_map = {1: 'loss',
              6: 'gain'}
 
 cytoband = pr.read_bed(snakemake.input.cytoband)
-cytoband = cytoband[~cytoband.Name.str.contains("_")]
-cb_arms = (cytoband.assign(arm=cytoband.Name.str[0])  # 'p' or 'q'
-              .df.groupby(["Chromosome", "arm"])[["Start", "End"]]
+cytoband = cytoband.subset(lambda x: ~x.Chromosome.str.contains("_"))
+cb_arms = (cytoband.assign('arm', lambda x: x.Name.str.split(".").str[0])
+              .df
+              .groupby(["Chromosome", "arm"])[["Start", "End"]]
               .agg({"Start":"min", "End":"max"})
+              .dropna(subset=["Start", "End"])
               .reset_index())
 cb_arms = pr.PyRanges(cb_arms)
 
 # Get all arm names
-arm_names = cb_arms.df.apply(lambda x: f"{x['Chromosome']}{x['arm']}", axis=1)
+arm_names = cb_arms.df.apply(lambda x: f"{x['Chromosome']} {x['arm']}", axis=1)
 
 for cell_grouping_file, gene_cnv_file in zip(
     snakemake.input.cell_grouping_files,
@@ -30,13 +32,15 @@ for cell_grouping_file, gene_cnv_file in zip(
     gene_cnv = pd.read_csv(gene_cnv_file, sep="\t")
 
     gene_cnv['state'] = gene_cnv['state'].map(state_map)
+    # Prepend chr
+    gene_cnv['chr'] = "chr" + gene_cnv['chr'].astype(str)
 
     cnv_pr = pr.PyRanges(gene_cnv.rename(columns={"chr": "Chromosome",
                                              "start": "Start", "end": "End"}))
-    annot = cnv_pr.join(cb_arms).df
-    annot["arm_id"] = annot["Chromosome"] + annot["arm"]
+    annot = cb_arms.join(cnv_pr, strandedness=False)
+    annot = annot.assign("arm_id", lambda x: x.Chromosome.astype(str) + " " + x.arm.astype(str))
 
-    summary = (annot.groupby(["cell_group_name", "arm_id"])
+    summary = (annot.df.groupby(["cell_group_name", "arm_id"])
                 .state
                 .agg(lambda x: x.mode()[0] if not x.mode().empty else "neutral")
                 .unstack()).reset_index()
